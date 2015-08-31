6d0dca7b8132a8ad820758a2c9ce448973d94443
adi <findadit@gmail.com>
2015-08-31 14:33:21 -0500

    Initial

---
+++ src/com/grublr/rest/SharePostHandler.java
@@ -0,0 +1,24 @@
+package com.grublr.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by adi on 8/31/15.
+ */
+
+//Sets the path to base URL + /hello
+@Path("/hello")
+public class SharePostHandler {
+
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello jersey" + "</title>"
+                + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+    }
+
+}
